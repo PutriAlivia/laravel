de496a86d34b0ae42f907931df40bff07adb547e
Putri Alivia <putrialivia666@gmail.com>
2021-03-30 14:19:19 +0700

    pembuatan dan pengisian tabel

---
+++ app/Models/toserba_alivia.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class toserba_alivia extends Model
+{
+    use HasFactory;
+}
