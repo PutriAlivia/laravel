373745e1f5ceb7a167e5c1557dd5c862b4dfba99
Putri Alivia <putrialivia666@gmail.com>
2021-03-30 15:30:47 +0700

    proses migrate setelah pengisian data melalui sedeer

--- app/Models/toserba_alivia.php
+++ app/Models/toserba_alivia.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\toserba_alivia as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class toserba_alivia extends Model
 {
     protected $table = 'toserba_alivia';
+    public $timestamps= false;
+    protected $primaryKey = 'id_barang';
+    /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori',
+        'harga',
+        'jumlah',
+    ];
 }
--- database/migrations/2021_03_30_071431_create_toserba_alivia_table.php
+++ database/migrations/2021_03_30_071431_create_toserba_alivia_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateToserbaAliviasTable extends Migration
+class CreateToserbaAliviaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateToserbaAliviasTable extends Migration
      */
     public function up()
     {
-        Schema::create('toserba_alivias', function (Blueprint $table) {
+        Schema::create('toserba_alivia', function (Blueprint $table) {
             $table->integer('id_barang');
             $table->string('kode_barang');
             $table->string('nama_barang');
@@ -31,6 +31,6 @@ class CreateToserbaAliviasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('toserba_alivias');
+        Schema::dropIfExists('toserba_alivia');
     }
 }
